4edfccc6f4e170f2015688a6af8e000d37ba9d8f
noahware <131519316+noahware@users.noreply.github.com>
2026-06-06 22:27:54 +0100

    Use stdint.h

--- src/apic_def.h
+++ src/apic_def.h
@@ -1,13 +1,5 @@
 #pragma once
-
-typedef signed char        int8_t;
-typedef short              int16_t;
-typedef int                int32_t;
-typedef long long          int64_t;
-typedef unsigned char      uint8_t;
-typedef unsigned short     uint16_t;
-typedef unsigned int       uint32_t;
-typedef unsigned long long uint64_t;
+#include <stdint.h>
 
 #ifdef _MSC_VER
 #pragma warning(push)
